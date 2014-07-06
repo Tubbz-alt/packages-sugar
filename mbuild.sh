77895bf7203dacef0cea5829d9b2d40a2477a049
Philip <philm@manjaro.org>
2014-07-06 09:47:07 +0200

    Try to fix branch issue in mbuild.sh

--- mbuild.sh
+++ mbuild.sh
@@ -7,6 +7,7 @@ export LC_MESSAGES=C
 CHROOT=/opt/manjarobuild
 ARCH=$(uname -m)
 BRANCH=unstable
+USER=$(ls ${CHROOT}/${BRANCH}-${ARCH} | cut -d' ' -f1 | grep -v root | grep -v lock)
 
 # do UID checking here so someone can at least get usage instructions
 if [ "$EUID" != "0" ]; then
@@ -14,7 +15,20 @@ if [ "$EUID" != "0" ]; then
     exit 1
 fi
 
+# Get host mirror
+host_mirror=$(pacman -Sddp extra/devtools 2>/dev/null | sed -E "s#(.*/)(.*/)extra/.*#\1${branch}/\$repo/\$arch#")
+if echo "${host_mirror}" | grep -q 'file://'; then
+	host_mirror_path=$(echo "${host_mirror}" | sed -E 's#file://(/.*)/\$repo/\$arch#\1#g')
+fi
+
 ${BRANCH}-${ARCH}-build -c -r ${CHROOT}
+
+echo "==> Fixing branch in chroot"
+echo "Server = ${host_mirror}" > ${CHROOT}/${BRANCH}-${ARCH}/root/etc/pacman.d/mirrorlist
+sed -i -e "s|^.*Branch=.*|Branch=${BRANCH}|" ${CHROOT}/${BRANCH}-${ARCH}/root/etc/pacman-mirrors.conf
+pacman -Syy -r ${CHROOT}/${BRANCH}-${ARCH}/root
+#pacman -Syy -r ${CHROOT}/${BRANCH}-${ARCH}/$USER
+echo "==> Done fixing branch in chroot"
 
 echo "==> Start building sugar"
 date
